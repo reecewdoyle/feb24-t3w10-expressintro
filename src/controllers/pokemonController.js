ef0a2572bed4fc3982adf91bd448a7a1cf6be0de
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-10-15 20:47:14 +1100

    fix: better naming of exports

--- src/controllers/pokemonController.js
+++ src/controllers/pokemonController.js
@@ -3,10 +3,14 @@ const express = require('express');
 // Create an instance of just a router, not a full server 
 const router = express.Router();
 
-
-
+router.get("/", (request, response) => {
+	response.json({
+		message:"Pokemon routes live here!"
+	});
+});
 
 // Pokemon API request 
+// localhost:3000/pokemon/random
 router.get("/random", async (request, response) => {
 	let pokemonData = {};
 
@@ -19,12 +23,6 @@ router.get("/random", async (request, response) => {
 	});
 });
 
-
-
-
-
 // Export the router for the app to use 
 // since we must tell the app instance to load up routers 
-module.exports = {
-	router
-}
+module.exports = router;
